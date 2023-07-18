eff013ab49ae9d8646513283bef9b1a6cf86ed70
Soukupp <1730342751@qq.com>
2023-07-18 16:40:43 +0800

    lab3 finished partly

--- kernel/proc.c
+++ kernel/proc.c
@@ -140,6 +140,12 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  } 
+  p->usyscall->pid = p->pid;
 
   return p;
 }
@@ -155,6 +161,9 @@ freeproc(struct proc *p)
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+  if(p->usyscall)
+    kfree((void*)p->usyscall);
+  p->usyscall = 0;
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
@@ -196,6 +205,13 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the sharedframe 
+  if(mappages(pagetable,USYSCALL,PGSIZE,
+              (uint64)(p->usyscall), PTE_R | PTE_U)<0){
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +222,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
